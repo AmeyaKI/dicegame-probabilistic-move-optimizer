dad0f910d81b4ff67cb85eaeab7bedf88092bced
AmeyaKI <akiwalkar@berkeley.edu>
2025-12-03 15:00:38 -0800

    first version of simulator func

--- simulator.py
+++ simulator.py
@@ -1,7 +1,8 @@
 import numpy as np
-import itertools as it
-from game_rules import RuleEvaluator
 from numpy import typing as npt
+from itertools import product
+from game_rules import RuleEvaluator
+import matplotlib.pyplot as plt
 
 """
 ULTIMATE GOAL: tell user the best move
@@ -16,14 +17,11 @@ ULTIMATE GOAL: tell user the best move
             Only preserve first roll_result in dictioanry, everything else keeps iterating till final # of pts reached
            
 """
+RULES = RuleEvaluator()
+DICE_TOTAL = 6
+ALL_DICE_COMBINATIONS = {i: [tuple(p) for p in product([1,2,3,4,5,6], repeat=i)] for i in range(7)}
 
-
-roll = np.random.randint(1, 7, 6) # NOTE: replace this with the inference result of the model
-begin = True
-
-rules = RuleEvaluator()
-
-def simulator(dice_rolls: npt.NDArray, starter=True):
+def simulator(dice_rolls: npt.NDArray):
     """_summary_
     Args:
         dice_rolls (npt.NDArray): numpy array of dice rolls
@@ -36,20 +34,63 @@ def simulator(dice_rolls: npt.NDArray, starter=True):
     Returns:
         Dict: {roll_result: [point distribution]}
     """
-
-    if len(dice_rolls) == 0:
-        return 0
-    
-    roll_results = rules.check_roll(dice_rolls)
-    if len(roll_results) == 0:
+  
+    initial_roll_results = RULES.check_roll(dice_rolls)
+    if len(initial_roll_results) == 0:
         return 0 # has to override previous rolls
+    scores_dict = {}
+    memoer = {}
+    
+    # actual recursive fucntion
+    def helper(remaining_dice: int, total_points: int): # calculate distribution of values for each roll_results
+        # Memo efficiency!
+        
+        key = (remaining_dice, total_points)
+        if key in memoer:
+            return memoer[key]
+        
+        totals = []
+        
+        # no dice left (NOTE: ignoring 6 dice reset rule)
+        if remaining_dice == 0:
+            totals.append(total_points) 
+            memoer[key] = totals
+            return totals
+        
+        for new_roll in ALL_DICE_COMBINATIONS[remaining_dice]:
+            new_roll = np.array(new_roll)
+            roll_results = RULES.check_roll(new_roll)
+            
+            if len(roll_results) == 0: # BUST Rule
+                totals.append(0)
+                continue # skip the below
+
+            for dice_value, num_dice, points in roll_results:
+                next_remaining_dice = remaining_dice - num_dice
+                next_points = total_points + points
+                
+                branch_totals = helper(next_remaining_dice, next_points) 
+                totals.extend(branch_totals)
+                
+        memoer[key] = totals
+        return totals
     
     
-    for dice_value, num_dice, points in roll_results:
-        ...
+    for dice_value, num_dice, points in initial_roll_results:
+        remaining_dice = DICE_TOTAL - num_dice
+        point_distribution = helper(remaining_dice, points)
+        
+        scores_dict[(dice_value, num_dice, points)] = point_distribution
+                
+        
+    return scores_dict
+
+
+
+
 
     
-def minimum_scores_evaluator(scores: dict, begin: bool = True):
+def low_score_remover(scores: dict, begin: bool = True):
     """Implements 350 and 1000 rule:
         if simulated_value: 0 points (begin=True) --> need to reach 1000 else 0 pts
         elif simulated_value: >1000 points (begin=False) -->  need to reach 350 pts else 0 pts
@@ -61,15 +102,31 @@ def minimum_scores_evaluator(scores: dict, begin: bool = True):
             if value_list[i] < minimum:  
                 value_list[i] = 0
         
-  
+
 def best_move(scores: dict):
     """Determine which move is the best by calculating statistics (tbd) from 
     each key's distribution in scores
 
     Args:
         scores (dict): _description_
+
     """
     ... 
     
+    
+    
+def main():
+    
+    dice_rolls = np.random.randint(1, 7, 6) # NOTE: replace this with the inference result of the model
+    begin = True
+    
+    scores_dict = simulator(dice_rolls)
+    
+    # low_score_remover(scores_dict, True)
+    
+    # best_move(scores_dict)
+    
+    
 
-
+if __name__ == '__main__':
+    main()
