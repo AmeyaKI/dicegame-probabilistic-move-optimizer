6d7b9c8e2c5034abf85da2e8eaf17ce77c790246
AmeyaKI <akiwalkar@berkeley.edu>
2025-12-11 15:01:17 -0800

    added img + bbox display to predict_dice() func

--- inference.py
+++ inference.py
@@ -2,7 +2,9 @@ import numpy as np
 import cv2
 import torch
 from ultralytics import YOLO # type: ignore
-import os, sys
+import os
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
 
 # 12/5/2025 NOTE: Works but model needs more diversified dataset
  
@@ -12,9 +14,9 @@ def load_model(weights_path):
     except FileNotFoundError as e:
         print(f"{e}: Model weights not found.")
     
-def custom_image_detector(image_dir):
+def find_custom_image(image_dir):
     try:
-        img = cv2.imread(image_dir)
+        img = Image.open(image_dir)
     except FileNotFoundError as e:
         print(f"{e}: Custom image not found.")
         
@@ -23,51 +25,63 @@ def custom_image_detector(image_dir):
     
     return img
 
-def predict_dice_values(model, img):
-    results = model(img)[0]
+
+def predict_dice(model, img, img_path):
+    # img = Image.open(img_path)
+    result = model(img_path)[0]
+    boxes = result.boxes.xyxy.cpu().numpy()
     
-    dice_scores = []
+    drawn_img = ImageDraw.Draw(img)
     
-    for det in results.boxes:
+    for x1, y1, x2, y2 in boxes:
+        drawn_img.rectangle([x1, y1, x2, y2], outline='blue', width=2)
+
+    dice_scores_raw = []
+    for det in result.boxes:
         pred_class = int(det.cls.cpu().numpy()) # predicted class
         confidence = float(det.conf.cpu().item()) # predidction confidence
         bbox = det.xyxy.cpu().numpy()[0] # bounding box
 
         x_center = (bbox[0] + bbox[2]) / 2
         
-        dice_scores.append((pred_class, confidence, x_center))
-    
-    if dice_scores is None:
+        dice_scores_raw.append((pred_class, confidence, x_center))
+        
+    if dice_scores_raw is None:
         return np.array([])
         
-    dice_scores.sort(key=lambda x: x[2]) # sort dice by order in which they appear in image (left to right) thru x_center
-    dice_values_final = [d[0] for d in dice_scores] # get dice pred_class values
+    dice_scores_raw.sort(key=lambda x: x[2]) # sort dice by order in which they appear in image (left to right) thru x_center
+    dice_values = np.array([d[0] for d in dice_scores_raw], dtype=np.int64) + 1 # get dice pred_class values
 
-    return np.array(dice_scores), np.array(dice_values_final, dtype=np.int64)
-
+    # Return: drawn img, dice_scores_raw, final dice_values 
+    return img, np.array(dice_scores_raw), dice_values
 
 def main():
-    weights = 'runs/yolo/dice_model_v1/weights/best.pt' # input("Enter path to weights: "")
-    custom_image_path = os.path.join(os.getcwd(), 'test_v1.jpg') # input("Enter path to custom image: ")
+    weights = 'runs/yolo/dice_model_v2/weights/best.pt' # input("Enter path to weights: "")
+    custom_image_path = os.path.join(os.getcwd(), 'sample_v4.jpg') # input("Enter path to custom image: ")
     
     model = load_model(weights)
     
     print("Loaded weights.")
     
-    img = custom_image_detector(custom_image_path)
+    img = find_custom_image(custom_image_path)
+    print("Image read.")
     
-    print("Read image.")
-    
-    dice_scores, dice_values = predict_dice_values(model, img)
-    
+    image, dice_scores_raw, dice_values = predict_dice(model, img, custom_image_path)
     print("Calculated dice values.")
+        
 
-    print(f"Holistic dice scores:\n{dice_scores}")
+    print(f"Holistic dice scores:\n{dice_scores_raw}")
     
     if dice_values.size == 0:
         print("No dice detected.")
     else:
         print(f"Detected dice values: {dice_values}")
+        
+    print("Displaying bounding boxes")
+
+    plt.imshow(image)
+    plt.axis('off')
+    plt.show()
 
 if __name__ == '__main__':
     main()
